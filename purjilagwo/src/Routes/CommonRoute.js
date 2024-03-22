8e1599876b498ce97f73a363d3417b65ebaecd1c
Vicky Kumar <ce21btech11045@iith.ac.in>
2024-03-22 23:14:07 +0530

    Added specialites round

--- purjilagwo/src/Routes/CommonRoute.js
+++ purjilagwo/src/Routes/CommonRoute.js
@@ -15,9 +15,9 @@ const EyeCare = lazy(() => import('../screens/SpecialistScreens/eyeCare/EyeCare'
 const Dentist = lazy(() => import('../screens/SpecialistScreens/dentist/Dentist'));
 const Cardiology = lazy(() => import('../screens/SpecialistScreens/cardiology/Cardiology'));
 const Ent = lazy(() => import('../screens/SpecialistScreens/ent/Ent'));
-// const Nephrology = lazy(() => import('../screens/nephrology/Nephrology'));
-// const Fertiltity = lazy(() => import('../screens/fertility/Fertiltity'));
-// const Paediatric = lazy(() => import('../screens/paediatric/Paediatric'));
+const Nephrology = lazy(() => import('../screens/SpecialistScreens/nephrology/Nephrology'));
+const Fertiltity = lazy(() => import('../screens/SpecialistScreens/fertility/Fertiltity'));
+const Paediatric = lazy(() => import('../screens/SpecialistScreens/paediatric/Paediatric'));
 // const PhysicanSurgeon = lazy(() => import('../screens/physican&surgeon/Physican&Surgeon'));
 
 export const commonroutes = [
@@ -73,22 +73,22 @@ export const commonroutes = [
     path: '/privacypolicy',
     element: <Privacy_policy />,
   },
-    {
+  {
     path: '/terms',
     element: <Terms />,
   },
-  // {
-  //   path: '/nephrology',
-  //   element: <Nephrology />,
-  // },
-  //  {
-  //    path: '/fertiltity',
-  //    element: <Fertiltity />,
-  //  },
-  //  {
-  //    path: '/paediatric',
-  //    element: <Paediatric />,
-  //  },
+  {
+    path: '/nephrology',
+    element: <Nephrology />,
+  },
+  {
+    path: '/fertiltity',
+    element: <Fertiltity />,
+  },
+  {
+    path: '/paediatric',
+    element: <Paediatric />,
+  },
   {
     path: '/ent',
     element: <Ent />,
